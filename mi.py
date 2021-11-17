d36cd4b5bdfdbb5f2465dfcef46fdb076bab5d93
Nilson99 <94497011+Nilson99@users.noreply.github.com>
2021-11-17 03:15:27 +0300

    Update mi.py

--- mi.py
+++ mi.py
@@ -1,1 +1,11 @@
-
+import random
+import time
+a = int(input())
+if a == 23:
+    for i in range(10):
+        time.sleep(5)
+        print('PUSH')
+        if i == 6:
+            print('STOP')
+            quit()
+        
